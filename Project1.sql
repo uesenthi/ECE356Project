25262589c371a43c23a2190c4f71509393ebac24
Alex Ohn <alexohn3026@gmail.com>
2016-11-14 20:05:29 -0500

    First opereation
    need more bois

--- Project1.sql
+++ Project1.sql
@@ -53,3 +53,29 @@ CREATE TABLE Friend_Requests(
 	FOREIGN KEY (alias_from) references Patient (alias) ON DELETE CASCADE,
 	FOREIGN KEY (alias_to) references Patient (alias) ON DELETE CASCADE
 );
+
+DROP PROCEDURE IF EXISTS CreatePatient;
+DELIMITER @@
+CREATE PROCEDURE Create Patient
+(	IN alias VARCHAR(20), IN province VARCHAR(30), IN city VARCHAR(50),
+ 	IN first_name VARCHAR(100), IN last_name VARCHAR(100), IN email VARCHAR(256)
+)
+BEGIN
+
+INSERT INTO Patient(
+	alias,
+	province,
+	city,
+	first_name,
+	last_name,
+	email
+	) VALUES (
+	alias,
+	province,
+	city,
+	first_name,
+	last_name,
+	email
+	);
+END @@
+DELIMITER;
